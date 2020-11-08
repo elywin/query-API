ff95c6eae1979735fbc021985c4787c132941cc3
elywin <lukeeraelywin@gmail.com>
2020-11-08 15:18:23 +0300

    ft(add:created database connection)

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+const postRoutes = require('./src/routes/posts');
+
+//databse connection
+mongoose.connect(process.env.dbURL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+});
+const db = mongoose.connection;
+
+//verify database connection
+db.once("open", (_) => {
+    console.log("databse connected:", process.env.dbURI);
+  });
+  
+  db.on("error", (err) => {
+    console.error("connection failed:", err);
+  });
+
 
 app.get('/',(req,res)=>{
     res.send('hello world!!');
