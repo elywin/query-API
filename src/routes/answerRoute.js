c3761b720b531a063e91bbd5ebd80acd906ecb3d
elywin <lukeeraelywin@gmail.com>
2020-11-30 13:01:03 +0300

    ft(Add:created get answer route

--- src/routes/answerRoute.js
+++ src/routes/answerRoute.js
@@ -3,3 +3,6 @@ const answer = require('../controllers/answerscontroller');
 
 //create answer
 router.post("questions/:id/answers",answer.post_ans);
+
+//view answers
+router.get("questions/:id/answers", answer.get_ans);
